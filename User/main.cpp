6eafaf432bbc91b65803c56bbff24ae90c8c8fec
LiferLifer <12112748@mail.sustech.edu.cn>
2022-12-03 17:58:10 +0800

    搬运.jpeg

--- User/main.cpp
+++ User/main.cpp
@@ -2,13 +2,13 @@
 
 #include "led.h"
 
-void Main(){
-	LED_GPIO_Config();
-
-	while (1) {
-		LED_ON();
-		for (int i = 0 ; i < 7200000; i ++);
-		LED_OFF();
-		for (int i = 0 ; i < 7200000; i ++);
-	}
+void Main() {
+    LED_GPIO_Config();
+//    LED_OFF();
+    while (1) {
+        LED_ON();
+        for (int i = 0; i < 720000; i++);
+        LED_OFF();
+        for (int i = 0; i < 720000; i++);
+    }
 }
